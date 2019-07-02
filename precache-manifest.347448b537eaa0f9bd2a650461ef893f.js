b7f35953de79b68c33ef93dccc06762ff47820f7
Jimmy Tang <jimmytang@Jimmyde-Mac-mini.local>
2019-07-02 14:47:31 +0800

    Updates

---
+++ precache-manifest.347448b537eaa0f9bd2a650461ef893f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05c8df9625fa055ff22a9275972f3673",
+    "url": "/react_redux_shopping_cart/index.html"
+  },
+  {
+    "revision": "731421374c805466b861",
+    "url": "/react_redux_shopping_cart/static/css/2.39bbabe8.chunk.css"
+  },
+  {
+    "revision": "76f4785b06768d014fc1",
+    "url": "/react_redux_shopping_cart/static/css/main.839a7fac.chunk.css"
+  },
+  {
+    "revision": "731421374c805466b861",
+    "url": "/react_redux_shopping_cart/static/js/2.56a3df85.chunk.js"
+  },
+  {
+    "revision": "76f4785b06768d014fc1",
+    "url": "/react_redux_shopping_cart/static/js/main.95238e25.chunk.js"
+  },
+  {
+    "revision": "b2d4a49aebdca74f0f64",
+    "url": "/react_redux_shopping_cart/static/js/runtime~main.5c96545e.js"
+  }
+]);
