5fe5c8d2cd08585652d8731964f906759be6fc9c
Jimmy Tang <jimmy@alp.com.tw>
2019-07-03 13:40:58 +0800

    title,

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -17,7 +17,7 @@ const user = (state=initialState,action) =>{
             return {
                 ...state,loggingIn:true,userInfo:{
                     status:"pendding",
-                    user
+                    userUser
                 }
             };
         
